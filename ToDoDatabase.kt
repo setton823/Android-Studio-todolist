1a39e9f9b3a9adccc9c107d962dfe3a2b39ded79
setton823 <97462865+setton823@users.noreply.github.com>
2022-02-03 14:52:28 +0900

    Add files via upload

---
+++ ToDoDatabase.kt
@@ -0,0 +1,9 @@
+package com.nishikawa.application.model.todo
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [ToDo::class], version = 1, exportSchema = false)
+ abstract class ToDoDatabase: RoomDatabase() {
+     abstract fun todoDAO(): ToDoDAO
+}
